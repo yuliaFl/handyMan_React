5c4c77b515a81218d62b104f66660a61255d55e2
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-07-01 23:42:00 -0400

    added esitmate button that redirects to contact page

--- proj/src/pages/Home.js
+++ proj/src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+function Home () {
+
+  const navigate = useNavigate();
+
+  const redirectPage=()=>{
+    navigate('/contact');
+  }
   return (
     <div className="home-background">
       <header>
         <h1>Ignats Website</h1>
       </header>
       <main>
+        <button id="estimate-button" onClick={redirectPage}>Get Your Free Estimate </button>
         <p>Hover over a part of the room that I can help you with</p>
       </main>
     </div>
