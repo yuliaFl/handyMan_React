b4e0d8911a1184968f4b9c916190432b9a8a6b2a
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-07-02 00:06:34 -0400

    added buttons for kitchen

--- proj/src/pages/Kitchen.js
+++ proj/src/pages/Kitchen.js
@@ -5,7 +5,9 @@ import './Kitchen.css';
 function Kitchen() {
 
   const sinkItems = ['Change Sink', 'Clogged Sink'];
-  const ovenItems = ['Change Sink', 'Clogged Sink'];
+  const backsplashItems = ['Fix', 'Change '];
+  const faucetItems = ['Replace'];
+  const doorItems = ['Fix ', 'Replace', 'Paint'];
 
   return (
     <div className="kitchen-background">
@@ -17,8 +19,14 @@ function Kitchen() {
         <div className="sink-section">
           <DropdownButton label="Sink" items={sinkItems} />
         </div>
-        <div className="oven-section">
-          <DropdownButton label="Oven" items={ovenItems} />
+        <div className="backsplash-section">
+          <DropdownButton label="backsplash" items={backsplashItems} />
+        </div>
+        <div className="faucet-section">
+          <DropdownButton label="faucet" items={faucetItems} />
+        </div>
+        <div className="door-section">
+          <DropdownButton label="door" items={doorItems} />
         </div>
       </main>
     </div>
