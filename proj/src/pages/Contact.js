80f3604c0b5a8e755c17b25cb142b3a89abe7e73
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-07-16 16:02:00 -0400

    added divs to adjust styling in contact

--- proj/src/pages/Contact.js
+++ proj/src/pages/Contact.js
@@ -5,17 +5,24 @@ function Contact() {
   return (
     <div className="home-background">
       <header>
-        <h1>Contact Page Unavailable</h1>
-        <h3>
+        <div className="page-header"> Contact Page Unavailable</div>
+        <div className="error-message">
           We're sorry, but our contact page is temporarily unavailable due to
-          maintenance.
-        </h3>
-        <h2>Please use the information below to reach me:</h2>
+          maintenance. Please use the information below to reach me:
+        </div>
         <div class="contact-info">
-          <h3>
-            Email: <a href="mailto:support@example.com" className="email-link" >support@example.com</a>
-          </h3>
-          <h3><a href="tel:+16479373558" className="phone-link" >+1 (647)937-3558</a></h3>
+          <br></br>
+          <br></br>
+          Email:{" "}
+          <a href="handyman.fixed.it@gmail.com" className="email-link">
+          handyman.fixed.it@gmail.com
+          </a>
+          <br></br>
+          <br></br>
+          Mobile:
+          <a href="tel:+16479373558" className="phone-link">
+            +1 (647)937-3558
+          </a>
         </div>
       </header>
     </div>
