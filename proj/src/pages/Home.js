74c8c2592568140c0e83a0f0a1490b991697c0f3
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-07-23 21:49:10 -0400

    changed css for home page to look better on phone

--- proj/src/pages/Home.js
+++ proj/src/pages/Home.js
@@ -10,31 +10,25 @@ function Home() {
   };
   return (
     <div className="home-background">
-      <header>
+    <div className="home-content">
         <div className="home-page-header">FixIt Handyman Solutions</div>
-      </header>
-      <main>
         <button id="estimate-button" onClick={redirectPage}>
-          Get Your Free Estimate{" "}
+          Get Your Free Estimate
         </button>
         <div class="handyman-description">
           <div class="title">
             Reliable Handyman Services for All Your Home Needs
           </div>
-          <div class="content">
+          <div className="content">
             From minor repairs to major renovations, we've got you covered! Our
-            skilled and experienced handyman is here to provide top-notch
-            services with a smile. Whether it's fixing a leaky faucet, painting
-            a room, or installing new fixtures, we handle it all with precision
-            and care. Your satisfaction is our priority, and no job is too big
-            or too small. Trust us to make your home the best it can be!
+            skilled handyman provides top-notch services with a smile. No job is too big or small.
           </div>
-          <div class="content">
+          <div className="content">
             Contact us today for a free estimate and let us take care of your
             home improvement projects!
           </div>
         </div>
-      </main>
+        </div>
     </div>
   );
 }
