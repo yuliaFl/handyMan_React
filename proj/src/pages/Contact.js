bb75f91a501548c29207f7433f298d400a06d2c2
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-07-24 09:22:04 -0400

    when screen is big the dropdown is on the right, when screen is small the dropdown is on the left. the dropdown background is dark now

--- proj/src/pages/Contact.js
+++ proj/src/pages/Contact.js
@@ -10,19 +10,9 @@ function Contact() {
           We're sorry, but our contact page is temporarily unavailable due to
           maintenance. Please use the information below to reach me:
         </div>
-        <div class="contact-info">
-          <br></br>
-          <br></br>
-          Email:{" "}
-          <a href="handyman.fixed.it@gmail.com" className="email-link">
-          handyman.fixed.it@gmail.com
-          </a>
-          <br></br>
-          <br></br>
-          Mobile:
-          <a href="tel:+16479373558" className="phone-link">
-            +1 (647)937-3558
-          </a>
+        <div className="contact-info">
+          <p>Email: <a href="mailto:handyman.fixed.it@gmail.com" className="email-link">handyman.fixed.it@gmail.com</a></p>
+          <p>Mobile: <a href="tel:+16479373558" className="phone-link">+1 (647)937-3558</a></p>
         </div>
       </header>
     </div>
