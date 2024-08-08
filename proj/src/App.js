a22a8e02deda5c6df0bc7bad150d7c387df92844
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-08-08 12:18:51 -0400

    scroll to the top function added

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './index.css';
-import Navbar from './pages/Navbar';
-import Home from './pages/Home';
-import Bathroom from './pages/Bathroom';
-import Bedroom from './pages/Bedroom';
-import Kitchen from './pages/Kitchen';
-import Contact from './pages/Contact';
-import Outdoor from './pages/Outdoor';
-import Footer from './pages/Footer';
-import About from './pages/About';
-import Reviews from './pages/Reviews';
-//import Reviews from './components/GoogleReviews';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import Bathroom from "./pages/Bathroom";
+import Bedroom from "./pages/Bedroom";
+import Kitchen from "./pages/Kitchen";
+import Contact from "./pages/Contact";
+import Outdoor from "./pages/Outdoor";
+import Footer from "./pages/Footer";
+import About from "./pages/About";
+import Reviews from "./pages/Reviews";
+import ScrollToTop from "./components/ScrollToTop";
 
 function MainPage() {
   return (
@@ -45,27 +45,21 @@ function MainPage() {
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/outdoor" element={<Outdoor />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+      <ScrollToTop />
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/outdoor" element={<Outdoor />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </Router>
+    </Router>
   );
 }
 
 export default App;
-
-
-/*
-    <div id="Reviews">
-        <Reviews />
-      </div>
-*/
